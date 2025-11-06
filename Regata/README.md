5ac0323a0e9e09fe905d780808ca9bc863d9c623
karlaM6 <karlam.martinezx@gmail.com>
2025-11-06 05:06:35 -0500

    Endpoints REST, repositorios, servicios, entidades y configuración CORS

--- Regata/README.md
+++
@@ -1,1 +0,0 @@
-# Regata-online-testing
--- Regata/regata-online/src/main/java/edu/tuuni/regata/repo/PlayerSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.tuuni.regata.repo;
-
-import edu.tuuni.regata.domain.PlayerSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerSessionRepository extends JpaRepository<PlayerSession, Long> {
-}
